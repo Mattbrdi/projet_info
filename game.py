7400af432afdc511ae7ee3e142d1fd30c72082a1
Amine <amine.amzai@etu.minesparis.psl.eu>
2024-02-09 20:04:07 +0100

    Amélioration de la syntaxe et ajout de commentaires

--- game.py
+++ game.py
@@ -2,57 +2,66 @@ import pygame
 import sys
 from Perso import personnage
 from Map import Map
-from portail import Portail, portail_select
+from portail import Portail, select_portal
 
 class Game: 
     def __init__(self):
         pass
          
-        
     def run(self, carte_filename):
         """Run the game"""
         carte = Map(carte_filename)
         perso = personnage(carte.spawn) 
         perso.regle_a_la_map(carte) #adapte certaines caracterisique du perso à la map
-        
         pygame.init()
         largeur, hauteur = carte.carre*len(carte.map[0]), carte.carre*len(carte.map)
         taille_fenetre = (largeur, hauteur)
         screen = pygame.display.set_mode(taille_fenetre)
         pygame.display.set_caption("screen")
         couleur_fond = (0,0,0)
+
+        #initialisation des portails 
         blue_portal = Portail(screen, carte_filename, 'blue')
         orange_portal = Portail(screen, carte_filename, 'orange')
-        blue_portal.is_selected = True
+        blue_portal.is_selected = True # Par défaut, le portail bleu est sélectionné
+
         running = True
         while running:
             #gerer l'évolution du jeu
-            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+    
             perso.force_applicated()
             touche = pygame.key.get_pressed()
-            portail_select(blue_portal, orange_portal, touche)
+
+            # Gestion des portails
+            select_portal(blue_portal, orange_portal, touche)
             if blue_portal.is_selected:
                 perso.handle_key_pressed(touche, blue_portal)
             elif orange_portal.is_selected:
                 perso.handle_key_pressed(touche, orange_portal)
 
+            # Gestion du personnage
             perso.next_position_considering_walls()
             perso.teleportation(carte, blue_portal, orange_portal)
 
-
-            #affichage     
+            #affichage de la carte  
             screen.fill(couleur_fond)
             carte.draw_map(screen)
+
+            #affichage du personnage
             perso.draw(screen)
+
+            #affichage des portails
             blue_portal.draw(screen, perso.position, touche)
             orange_portal.draw(screen, perso.position, touche)
             if blue_portal.is_placed:
                 blue_portal.keep_drawing(screen)
             if orange_portal.is_placed:
                 orange_portal.keep_drawing(screen)
+                
+            #affichage de l'écran
             pygame.event.pump()
             pygame.display.flip()
             pygame.time.Clock().tick(60)
--- portail.py
+++ portail.py
@@ -1,47 +1,84 @@
-import pygame 
+import pygame
 
-def portail_select(portail_1, portail_2, touches):
-    assert portail_1.is_selected != portail_2.is_selected
-    if touches[pygame.K_TAB]:
-        if portail_1.is_selected:
-            portail_1.is_selected = False
-            portail_2.is_selected = True
+
+def select_portal(portal_1, portal_2, keys):
+    """
+    Permet de sélectionner un portail à la fois
+    """
+    assert portal_1.is_selected != portal_2.is_selected
+    if keys[pygame.K_TAB]:
+        if portal_1.is_selected:
+            portal_1.is_selected = False
+            portal_2.is_selected = True
         else:
-            portail_1.is_selected = True
-            portail_2.is_selected = False
+            portal_1.is_selected = True
+            portal_2.is_selected = False
 
 
 def choose_color(color):
+    """
+    Choix de la couleur du portail
+    """
     if color == 'blue':
         return (0, 255, 255)
     if color == 'orange':
         return (255, 100, 0)
 
+
 def get_tile_normal(grid, x, y):
-    if x > 0 and grid[y][x - 1] == '.':  # There's an empty tile to the left
+    """
+    Renvoie la direction de la surface du portail qui est la normale à la surface
+    sur laquelle le portail est placé
+    """
+    if x > 0 and grid[y][x - 1] == '.':  # Il y a une case vide à gauche
         return (-1, 0)
-    elif x < len(grid[0]) - 1 and grid[y][x + 1] == '.':  # There's an empty tile to the right
+    elif x < len(grid[0]) - 1 and grid[y][x + 1] == '.':  # Il y a une case vide à droite
         return (1, 0)
-    elif y > 0 and grid[y - 1][x] == '.':  # There's an empty tile above
+    elif y > 0 and grid[y - 1][x] == '.':  # Il y a une case vide au dessus
         return (0, -1)
-    elif y < len(grid) - 1 and grid[y + 1][x] == '.':  # There's an empty tile below
+    elif y < len(grid) - 1 and grid[y + 1][x] == '.':  # Il y a une case vide en dessous
         return (0, 1)
     else:
-        return (-1, 0)  # The tile is surrounded by other tiles
+        return (-1, 0)  # Si aucune case vide n'est trouvée, on retourne la normale par défaut
+    
 
 def dilatation(text_file, factor):
+    """
+    Cette fonction prend un fichier texte qui représente la map et le dilate par 
+    un facteur donné. Nous en aurons besoin pour pouvoir placer les portails sur la carte
+    
+    Parameters : 
+    text_file (.txt) : le fichier texte qui représente la map
+    factor (int) : le facteur de dilatation
+
+    Returns : 
+    array (list) : la map dilatée
+    """
     with open(text_file, 'r') as file:
         array = [list(line.strip().replace('@', '.')) for line in file] 
     return [[char for char in row for _ in range(factor)] for row in array for _ in range(factor)]
 
-#Correction du placement du portail en fonction de l'orientation de ce dernier
+"""
+Ce dictionnaire permet de corriger le placement des portails sur la carte en fonciton 
+de l'orientation de ces derniers
+"""
 pos = {'up':     [-29, 25 , 60, 14], 
        'down':   [-29, -38, 60, 14], 
        'left':   [25, -28, 14, 60], 
         'right': [-38, -28, 14, 60],
       }
  
-class Portail: 
+class Portail:
+    """
+    La classe portail permet de créer un portail, de la placer sur la carte 
+    et de le déplacer
+
+    Parameters : 
+
+    screen (pygame.Surface) : la fenêtre de jeu
+    carte (str) : le fichier texte qui représente la map
+    color (str) : la couleur du portail
+    """
     def __init__(self, screen, carte, color):
         self.x = 0 
         self.y = 0 
@@ -55,16 +92,24 @@ class Portail:
     
     #TODO : the int position may not be the position of the character
     def get_draw_position(self, touches, position):
-        int_position = [int(position[0]),int(position[1])]
+        """
+        Cette fonction permet de placer le portail sur la carte en fonction de la
+        direction dans laquelle le joueur veut le placer.
+
+        Parameters :
+        touches (dict) : les touches du clavier appuyées
+        position (list) : la position du personnage
+
+        Returns :
+        int_position (list) : la position du portail sur la carte
+        """
+        int_position = [int(position[0]), int(position[1])]
         i = int_position[1]
         j = int_position[0]
         max_i = len(self.map) - 1
         max_j = len(self.map[0]) - 1
-        #print(i , j)
         if touches[pygame.K_z] and not touches[pygame.K_q] and not touches[pygame.K_d] and not touches[pygame.K_s]:
-            #print(i,j,  len(self.map), len(self.map[0]))
             while self.map[i][j] == '.' and i >= 0:
-                #print(self.map[i][j])
                 i -= 1
         elif touches[pygame.K_s] and not touches[pygame.K_q] and not touches[pygame.K_d] and not touches[pygame.K_z]:
             while self.map[i][j] == '.' and i <= max_i:
@@ -92,20 +137,39 @@ class Portail:
                 i += 1
                 j += 1
         return j, i
-    
+
     def get_portal_facing(self, surface_normal):
-        if surface_normal == (0, -1):  
+        """
+        Cette fonction permet de déterminer l'orientation du portail en fonction de la
+        normale de la surface sur laquelle il est placé.
+
+        Parameters :
+        surface_normal (tuple) : la normale de la surface sur laquelle le portail est placé
+
+        Returns :
+        str : l'orientation du portail
+        """
+        if surface_normal == (0, -1):
             return 'up'
-        elif surface_normal == (0, 1):  
+        elif surface_normal == (0, 1):
             return 'down'
-        elif surface_normal == (-1, 0):  
+        elif surface_normal == (-1, 0):
             return 'left'
         elif surface_normal == (1, 0):
             return 'right'
         else:
-            return 'none'  # Surface normal doesn't match any known direction
+            return 'left'  # Si aucune case vide n'est trouvée, on retourne la normale par défaut
 
     def draw(self, screen, position, touches):
+        """
+        Cette fonction permet de dessiner le portail sur la carte en fonction de la
+        direction dans laquelle le joueur veut le placer.
+        
+        Parameters :
+        screen (pygame.Surface) : la fenêtre de jeu
+        position (list) : la position du personnage
+        touches (dict) : les touches du clavier appuyées
+        """
         if self.is_selected:
             if touches[pygame.K_SPACE]:
                 self.x, self.y = self.get_draw_position(touches, position)
@@ -115,6 +179,15 @@ class Portail:
                     self.keep_drawing(screen)
                     
     def keep_drawing(self, screen):
+        """
+        Cette fonction permet de garder le portail dessiné sur la carte
+        Parameters:
+        screen (pygame.Surface) : la fenêtre de jeu
+
+        Note:
+        Cette fonciton utilise le dictionnaire 'pos' pour corriger le placement du portail 
+        en fonction de l'orientation de ce dernier
+        """
         rect_ovale = pygame.Rect(self.x + pos[self.facing][0], self.y + pos[self.facing][1], pos[self.facing][2], pos[self.facing][3])
         pygame.draw.ellipse(screen, self.drawing_color, rect_ovale)
 
