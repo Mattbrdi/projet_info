dc7e28358b9804f57afe8f72678824f36a6db981
Amine <amine.amzai@etu.minesparis.psl.eu>
2024-02-09 16:29:26 +0100

    commit sur amine4

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from Perso import personnage
 from Map import Map
+from portail import Portail, portail_select
 
 class Game: 
     def __init__(self):
@@ -20,7 +21,9 @@ class Game:
         screen = pygame.display.set_mode(taille_fenetre)
         pygame.display.set_caption("screen")
         couleur_fond = (0,0,0)
-
+        blue_portal = Portail(screen, carte_filename, 'blue')
+        orange_portal = Portail(screen, carte_filename, 'orange')
+        blue_portal.is_selected = True
         running = True
         while running:
             #gerer l'évolution du jeu
@@ -30,14 +33,25 @@ class Game:
                     running = False
             perso.force_applicated()
             touche = pygame.key.get_pressed()
-            perso.handle_key_pressed(touche)
+            portail_select(blue_portal, orange_portal, touche)
+            if blue_portal.is_selected:
+                perso.handle_key_pressed(touche, blue_portal)
+            elif orange_portal.is_selected:
+                perso.handle_key_pressed(touche, orange_portal)
+
             perso.next_position_considering_walls()
+
             #affichage     
             screen.fill(couleur_fond)
             carte.draw_map(screen)
             perso.draw(screen)
+            blue_portal.draw(screen, perso.position, touche)
+            orange_portal.draw(screen, perso.position, touche)
+            if blue_portal.is_placed:
+                blue_portal.keep_drawing(screen)
+            if orange_portal.is_placed:
+                orange_portal.keep_drawing(screen)
             pygame.event.pump()
-
             pygame.display.flip()
             pygame.time.Clock().tick(60)
     
